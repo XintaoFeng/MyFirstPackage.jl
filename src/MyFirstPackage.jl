db01d592f954784fe8c84938534d9b0a9b79c8df
xintaofeng <xfeng260@connect.hkust-gz.edu.cn>
2024-03-13 20:28:07 +0800

    upload

--- src/MyFirstPackage.jl
+++ src/MyFirstPackage.jl
@@ -6,8 +6,7 @@ using LinearAlgebra
 export Lorenz, integrate_step
 export Point, Point2D, Point3D
 export RungeKutta, Euclidean
-export D2Q9, directions, Cell, density
-export momentum
+export D2Q9, LatticeBoltzmann, step!, equilibrium_density, momentum, curl, example_d2q9, density
 
 # `include` other source files into this module
 include("lorenz.jl")
